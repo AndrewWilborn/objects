4fde599af92566a8ea291a741c93adea77b67f0d
Andrew Wilborn <andrewwilborn@Andrews-MacBook-Air.local>
2023-06-08 17:34:39 -0400

    solved bonus problem

--- objects.js
+++ objects.js
@@ -7,3 +7,12 @@ console.log(constructor("fish", 24))
 function findNthElement(array, N){
     return array[N + 1];
 }
+
+let array = []
+
+function constructorToArray(arg1, arg2){
+    array.push({arg1: arg1, arg2: arg2})
+}
+constructorToArray("cat", 100)
+
+console.log(array)
